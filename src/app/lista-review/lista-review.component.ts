508ce5d35705cfc87615827a5e86f78060e54214
Simo Confa <simone97confalonieri@gmail.com>
2023-02-08 16:59:31 +0100

    Creazione lista-review

---
+++ src/app/lista-review/lista-review.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Review } from '../model/review';
+import { ReviewsService } from '../service/reviews.service';
+
+@Component({
+  selector: 'app-lista-review',
+  templateUrl: './lista-review.component.html',
+  styleUrls: ['./lista-review.component.css'],
+})
+export class ListaReviewComponent implements OnInit {
+  listaReview!: Review[];
+
+  constructor(private reviewsService: ReviewsService) {}
+
+  ngOnInit(): void {
+    this.listaReview = this.reviewsService.getReviews();
+  }
+}
