375df4e415bf4109a81181945af6e14ff69101ff
FlyingCorvette <123636144+FlyingCorvette@users.noreply.github.com>
2023-02-10 09:19:10 +0100

    Commentato ngFor

--- src/app/service/videogiochi.service.ts
+++ src/app/service/videogiochi.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class VideogiochiService{
-    private apiUrl = 'https://project-works-rest-api.onrender.com/api/v1/GROUP-II/videogames';
+    private apiUrl = 'https://project-works-rest-api.onrender.com/api/v1/GROUP-II/videogame';
 
     constructor(private http: HttpClient) {}
 
