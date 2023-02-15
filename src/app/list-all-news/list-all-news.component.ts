9c6025d5800e0b77159bcbbf4eb9e792f329560e
Nicolò Trombin <nikitrombin744@gmail.com>
2023-02-15 17:58:54 +0100

    Inizio pagina list-all-news

--- src/app/list-all-news/list-all-news.component.ts
+++ src/app/list-all-news/list-all-news.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { New } from '../model/news';
+import { NewsService } from '../service/news.service';
 
 @Component({
   selector: 'app-list-all-news',
   templateUrl: './list-all-news.component.html',
   styleUrls: ['./list-all-news.component.css']
 })
-export class ListAllNewsComponent {
+export class ListAllNewsComponent implements OnInit{
+
+  listaNew$ !: Observable<New[]>;
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.listaNew$ = this.newsService.getNews();
+  }
+
 
 }
