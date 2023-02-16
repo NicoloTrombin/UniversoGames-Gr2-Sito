b4528cf75d92f38b33c4921c7f95ae44d033d338
Nicolò Trombin <nikitrombin744@gmail.com>
2023-02-16 10:17:11 +0100

    Modifiche generiche nella lista news
    Implementato bottone to-the-top e modifiche generiche

--- src/app/lista-news/lista-news.component.ts
+++ src/app/lista-news/lista-news.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { New } from '../model/news';
 import { Videogioco } from '../model/videogiochi';
@@ -21,5 +21,32 @@ export class ListaNewsComponent implements OnInit{
     this.listaNew$ = this.newsService.getNews()
     this.listaVideogiochi$ = this.videogiochiService.getVideogiochi();
   }
-  
+
+  isShow: boolean = false;
+  topPosToStartShowing = 100;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
--- src/app/lista-videogiochi/lista-videogiochi.component.ts
+++ src/app/lista-videogiochi/lista-videogiochi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Videogioco, Linguaggio } from '../model/videogiochi';
 import { VideogiochiService } from '../service/videogiochi.service';
 import { Observable } from 'rxjs';
@@ -18,5 +18,33 @@ export class ListaVideogiochiComponent {
   ngOnInit(): void {
     this.listaVideogiochi$ = this.videogiochiService.getVideogiochi();
     console.log(this.listaVideogiochi$);
+  }
+
+  isShow: boolean = false;
+  topPosToStartShowing = 100;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 }
